e0ecde4b2f01e7ef04a2f1cdf2c9ec5e6e20adea
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-09-06 23:13:05 +0530

    updated with patches

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react"
 import "./Navbar.css"
 import {AiFillCloseCircle} from "react-icons/ai"
 import {FaBars} from "react-icons/fa"
+import {IoClose} from "react-icons/io"
 
 function Navbar (){
     const navref = useRef();
@@ -19,7 +20,7 @@ function Navbar (){
                 <a className="m" onClick={shownavref} href="#contact">Contact</a>
                 <a className="m" onClick={shownavref} href="https://www.canva.com/design/DAE8Y4Nu5TY/ikBgov_AYxtQITyanCbigA/view?utm_content=DAE8Y4Nu5TY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank">Resume</a>
                 <button className="nav-btn nav-close-btn" onClick={shownavref}>
-                    <AiFillCloseCircle/>
+                    <IoClose/>
                 </button>
             </nav>
             <button className="nav-btn" onClick={shownavref}>
