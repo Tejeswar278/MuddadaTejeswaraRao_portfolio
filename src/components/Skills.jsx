2dc8f65d1934cc85e9c4f9fe29b49e3467ea931f
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-09-07 14:42:48 +0530

    updated icons in skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,35 +1,42 @@
 import './Skills.css'
+import {BiLogoMongodb,BiLogoReact,BiLogoNodejs,BiLogoJavascript} from "react-icons/bi"
+import {GrMysql} from "react-icons/gr"
+import {SiFastify,SiTypescript,SiJavascript,SiHtml5,SiCss3,SiPostman,SiAmazonaws,SiGitlab,SiJenkins} from "react-icons/si"
+import {TbBrandVscode} from "react-icons/tb"
 
-export const Skills = () => {
+function Skills(){
     return (
         <div id='skills' className="skills-container offset">
             {/* <div className='m-t dot'>.</div> */}
             <div className='m-t skills-header'>Skills & <span>proficiencies</span></div>
             <div className='skill m-b'>
                 <div className='skills-btns'>
-                    <button className='skills-btn'>Mongodb</button>    
-                    <button className='skills-btn'>Express</button>
-                    <button className='skills-btn'>React</button>
-                    <button className='skills-btn'>Nodejs</button>
-                    <button className='skills-btn'>MySQL</button>
-                    <button className='skills-btn'>Fastify</button>
+                    <button className='skills-btn'><BiLogoMongodb/>  Mongodb</button>    
+                    <button className='skills-btn'>Express.js</button>
+                    <button className='skills-btn'><BiLogoReact/>  React</button>
+                    <button className='skills-btn'><BiLogoNodejs/>  Node.js</button>
+                    <button className='skills-btn'><GrMysql/>  MySQL</button>
+                    <button className='skills-btn'><SiFastify/>  Fastify</button>
                 </div>
                 <div>
-                    <button className='skills-btn'>Javascript</button>
-                    <button className='skills-btn'>Typescript</button>
-                    <button className='skills-btn'>HTML5</button>
-                    <button className='skills-btn'>CSS3</button>
+                    <button className='skills-btn'><SiJavascript/>  Javascript</button>
+                    <button className='skills-btn'><SiTypescript/>  Typescript</button>
+                    <button className='skills-btn'><SiHtml5/>  HTML5</button>
+                    <button className='skills-btn'><SiCss3/>  CSS3</button>
                     
                 </div>
             </div>
-            <div className='skill'>
-                    <button className='skills-btn'>Visual studio</button>
-                    <button className='skills-btn'>Postman</button>
-                    <button className='skills-btn'>Amazonaws</button>
-                    <button className='skills-btn'>Gitlab</button>
+            <div className='m-t tools-header skills-header'>Tools</div>
+            <div className='skill tools'>
+                    <button className='skills-btn'><TbBrandVscode/>  Visual studio</button>
+                    <button className='skills-btn'><SiPostman/>  Postman</button>
+                    <button className='skills-btn'><SiAmazonaws/>  AWS</button>
+                    <button className='skills-btn'><SiGitlab/>  Gitlab</button>
                     <button className='skills-btn'>SQLyog</button>
-                    <button className='skills-btn'>Jenkins</button>
+                    <button className='skills-btn'><SiJenkins/>  Jenkins</button>
             </div>
         </div>
     )
 }
+
+export default Skills
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -5,10 +5,10 @@ import {FaBars} from "react-icons/fa"
 
 function Sidebar (){
     return (
-        <div class="icon-bar">
-            <a href="https://github.com/Tejeswar278" class="icon-style" target="_blank" className="icon-style"><i class="fa fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/tejeswara-rao-muddada/" target="_blank" class="icon-style"><i class="fa fa-linkedin"></i></a>
-            <a href="https://twitter.com/Tejeswa07052907" target="_blank" class="icon-style"><i class="fa fa-twitter"></i></a>
+        <div className="icon-bar">
+            <a href="https://github.com/Tejeswar278" className="icon-style" target="_blank"><i className="fa fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/tejeswara-rao-muddada/" target="_blank" className="icon-style"><i className="fa fa-linkedin"></i></a>
+            <a href="https://twitter.com/Tejeswa07052907" target="_blank" className="icon-style"><i className="fa fa-twitter"></i></a>
         </div>
     )
 }
