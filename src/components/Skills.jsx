b13cf3755a2e8cb213a8393e6146d0d3b4819eea
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-09-07 15:16:34 +0530

    updated skills css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,29 +11,29 @@ function Skills(){
             <div className='m-t skills-header'>Skills & <span>proficiencies</span></div>
             <div className='skill m-b'>
                 <div className='skills-btns'>
-                    <button className='skills-btn'><span><BiLogoMongodb/></span><br/>  Mongodb</button>    
-                    <button className='skills-btn'><span><SiExpress/></span><br/>  Express.js</button>
-                    <button className='skills-btn'><span><BiLogoReact/></span><br/>  React</button>
-                    <button className='skills-btn'><span><BiLogoNodejs/></span><br/>  Node.js</button>
-                    <button className='skills-btn'><span><GrMysql/></span><br/>  MySQL</button>
-                    <button className='skills-btn'><span><SiFastify/></span><br/>  Fastify</button>
+                    <button className='skills-btn'><span className='mongo'><BiLogoMongodb/></span><br/>  Mongodb</button>    
+                    <button className='skills-btn'><span className='express'><SiExpress/></span><br/>  Express.js</button>
+                    <button className='skills-btn'><span className='react'><BiLogoReact/></span><br/>  React</button>
+                    <button className='skills-btn'><span className='node'><BiLogoNodejs/></span><br/>  Node.js</button>
+                    <button className='skills-btn'><span className='sql'><GrMysql/></span><br/>  MySQL</button>
+                    <button className='skills-btn'><span className='fastify'><SiFastify/></span><br/>  Fastify</button>
                 </div>
                 <div>
-                    <button className='skills-btn'><span><SiJavascript/></span><br/>  Javascript</button>
-                    <button className='skills-btn'><span><SiTypescript/></span><br/>  Typescript</button>
-                    <button className='skills-btn'><span><SiHtml5/></span><br/>  HTML5</button>
-                    <button className='skills-btn'><span><SiCss3/></span><br/>  CSS3</button>
+                    <button className='skills-btn'><span className='javascript'><SiJavascript/></span><br/>  Javascript</button>
+                    <button className='skills-btn'><span className='typesccript'><SiTypescript/></span><br/>  Typescript</button>
+                    <button className='skills-btn'><span className='html'><SiHtml5/></span><br/>  HTML5</button>
+                    <button className='skills-btn'><span className='css'><SiCss3/></span><br/>  CSS3</button>
                     
                 </div>
             </div>
             <div className='m-t tools-header skills-header'>Tools</div>
             <div className='skill tools'>
-                    <button className='skills-btn'><span><TbBrandVscode/></span><br/>  Visual studio</button>
-                    <button className='skills-btn'><span><SiPostman/></span><br/>  Postman</button>
-                    <button className='skills-btn'><span><SiAmazonaws/></span><br/>  AWS</button>
-                    <button className='skills-btn'><span><SiGitlab/></span><br/>  Gitlab</button>
+                    <button className='skills-btn'><span className='visualstudio'><TbBrandVscode/></span><br/>  Visual studio</button>
+                    <button className='skills-btn'><span className='postman'><SiPostman/></span><br/>  Postman</button>
+                    <button className='skills-btn'><span className='aws'><SiAmazonaws/></span><br/>  AWS</button>
+                    <button className='skills-btn'><span className='gitlab'><SiGitlab/></span><br/>  Gitlab</button>
                     <button className='skills-btn'>SQLyog</button>
-                    <button className='skills-btn'><span><SiJenkins/></span><br/>  Jenkins</button>
+                    <button className='skills-btn'><span className='jenkins'><SiJenkins/></span><br/>  Jenkins</button>
             </div>
         </div>
     )
