b82c851c20f5a6731877a37b6b2472622e73c237
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-09-07 15:23:00 +0530

    checking dial pad

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
             <div className="m-t contact-header">Contact <span>me</span></div>
             <div className="contact-sub-header">Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you.</div>
             <div className="contact-list">
-                <a onclick="window.open('tel:8333014386')">8333014386</a>
+                <a href="window.open('tel:8333014386')">8333014386</a>
                 <a className="contact-m" href="mailto:muddadateja1999@gmail.com" target="_blank">muddadateja1999@gmail.com</a>
                 <a className="contact-g" href="https://github.com/Tejeswar278" target="_blank">Github</a>
                 <a className="contact-l" href="https://www.linkedin.com/in/tejeswara-rao-muddada/" target="_blank">LinkedIn</a>
